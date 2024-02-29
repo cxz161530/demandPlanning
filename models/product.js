59aa08de09cd4aba4eb8ba56891d74d58009f933
cxz161530 <czzhu940222@gmail.com>
2024-02-29 09:01:38 -0800

    DONE DEMAND ADD FUNCTION

--- models/product.js
+++ models/product.js
@@ -35,7 +35,7 @@ const productSchema= new Schema({
         
       }],
 
-    Demands: [demandSchema]
+    demands: [demandSchema]
 },{
     timestamps: true
 })
--- routes/demands.js
+++ routes/demands.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
 const demandsCtrl = require('../controllers/demands')
 
-router.post('/products/:id/demands', demandsCtrl.create)
+router.post('/products/:id/demand', demandsCtrl.create)
 
 module.exports = router;
